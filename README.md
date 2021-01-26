a9369b1d431b7d109113bb66a90e6c5d3e24e317
Andiepanasenko <panasenko.oleksii@pdffiller.team>
2021-01-26 10:18:15 +0200

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WPbackup-script
+'''
+Install required Linux packages
+'''
+
+[sudo] pip3 install -r req.txt
---
+++ WPengine_backup.py
@@ -0,0 +1,63 @@
+import requests
+from tqdm import tqdm
+import boto3
+from boto3.s3.transfer import TransferConfig
+import re
+import sys
+
+
+lst = sys.argv[1:]
+s3 = boto3.resource('s3')
+
+# Set the desired multipart threshold value (13GB)
+GB = 1024 ** 3
+transfer_config = TransferConfig(multipart_threshold=13 * GB, max_concurrency=4)
+# def url_list(file_name):
+#     with open(file_name, "r") as file:
+#         l = file.readlines()
+#         return l
+
+def processing_backup(item, config):
+    """ for item in lst:"""
+    pattern = re.search(r'\/([a-zA-Z0-9-]+\.zip)', item)
+    if pattern is None:
+        print("url does not match")
+        return "not completed"
+    article = re.sub(r'-[a-zA-Z0-9]+(:?\.zip)', '.zip', pattern.group(1))
+    print("start process %s" % article)
+    if 'snblog-live' in article:
+        folder = "blog.signnow.com/"
+    elif "signnow2016-live" in article:
+        folder = "university.signnow.com/"
+    elif 'airslate-live' in article:
+        folder = "blog.airslate.com/"
+    elif 'pdffillerblog-live' in article:
+        folder = "blog.pdffiller.com/"
+    elif 'sellmyforms-live' in article:
+        folder = "blog.sellmyforms.com/"
+    elif 'uslegal-live' in article:
+        folder = "uslegal.com/"
+    else:
+        folder = "unknown folder"
+        raise ValueError(folder)
+    response = requests.get(item, stream=True)
+
+    with open(article, "wb") as file:
+        for chunk in tqdm(response.iter_content(chunk_size=5000000*20)):
+            print("got chunk response")
+            if chunk:
+                file.write(chunk)
+
+    print(article)
+    s3.meta.client.upload_file(
+        Bucket='mr-prod-wpengine-backup-20201022121325811200000001',
+        Config=config,
+        Key=folder + article,
+        Filename=article
+        )
+    print("complete")
+
+
+for i in lst:
+    processing_backup(i, transfer_config)
+
